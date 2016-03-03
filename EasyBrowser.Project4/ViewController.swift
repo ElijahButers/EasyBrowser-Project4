a6a71348a0f4b2698ced651174fe25440ca9d230
ElijahButers <Elijahbuters@gmail.com>
2016-03-03 16:22:17 +0200

    Added var webView: WKWebView.

--- EasyBrowser.Project4/ViewController.swift
+++ EasyBrowser.Project4/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
